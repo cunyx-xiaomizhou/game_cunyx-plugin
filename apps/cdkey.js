aeea6170542737b5fe69035ad3a099c49cf743ef
最好喝的小米粥 <12395046+cunyx@user.noreply.gitee.com>
2023-06-27 12:26:38 +0000

    更新了非主人也可使用兑换码的bug
    Signed-off-by: 最好喝的小米粥 <12395046+cunyx@user.noreply.gitee.com>

--- apps/cdkey.js
+++ apps/cdkey.js
@@ -20,6 +20,7 @@ export class cunyx_cdkey extends plugin {
   async cdkey (e) {
     if (!e.isMaster) {
       e.reply('只有主人才能使用兑换码哦~');
+      return true;
     }
     let cdkey = e.msg.replace(/(寸幼萱|(c|C)(u|U)?(n|N)?(y|Y)(x|X))兑换(码)?|#/g, '').trim();
     let json = await fetch(`http://api.cunyx.cn/Yunzai-Bot/cdkey.php?qq=${data.qq}&token=${data.api}&cdkey=${cdkey}`);
