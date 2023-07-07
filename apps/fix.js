4141c18e7a4eec5a978f4a71a9a4644c2a2ce76d
最好喝的小米粥 <12395046+cunyx@user.noreply.gitee.com>
2023-07-07 06:08:23 +0000

    update apps/fix.js.
    Signed-off-by: 最好喝的小米粥 <12395046+cunyx@user.noreply.gitee.com>

--- apps/fix.js
+++ apps/fix.js
@@ -1,4 +1,5 @@
 import plugin from './../../../lib/plugins/plugin.js';
+import fs from 'fs';
 export class path_ extends plugin {
   constructor () {
     super({
@@ -7,11 +8,20 @@ export class path_ extends plugin {
       event:"message",
       priority:1,/*优先级*/
       rule:[
-        {reg:"^#?机器人根目录",fnc:"path_",}
+        {reg:"^#?机器人根目录",fnc:"path_"},
+        {reg:"^#?(a|A)(p|P)(i|I)文件内容",fnc:"api_text"}
       ]
     });
   }
   async path_ (e) {
     e.reply(process.cwd());
+  }
+  async api_text (e) {
+    if (e.group_id) {
+      e.reply('请在私聊使用');
+      return true;
+    }
+    let text = fs.readFileSync('./config/cunyx_api.yaml','utf-8');
+    e.reply(api_text);
   }
 }
