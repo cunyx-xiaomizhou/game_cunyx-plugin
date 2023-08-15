3c7acf0eddadd0f6616b11c8c17001a65c31ecab
最好喝的小米粥 <admin@cunyx.cn>
2023-08-15 02:07:08 +0000

    新功能 你猜一下是麻功能吧
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

---
+++ apps/cpl.js
@@ -0,0 +1,40 @@
+import plugin from './../../../lib/plugins/plugin.js';
+import fetch from 'node-fetch';
+import { segment } from 'oicq';
+import YAML from 'yaml';
+import fs from 'fs';
+let qq;
+let data = YAML.parse(fs.readFileSync('./plugins/cunyx-plugin/config/cunyx_api.yaml','utf-8'));
+export class plugin_name extends plugin {
+  constructor () {
+    super({
+      name:"土味情话",
+      dsc:"来啊撩我啊",
+      event:"message",
+      priority:1,/*优先级*/
+      rule:[
+        {reg:"^#?(撩(我)?|土味情话)",fnc:"cheesy_pick_up_lines"}
+      ]
+    });
+  }
+  async cheesy_pick_up_lines (e) {
+    if (e.msg.includes('我')) {
+      qq = e.user_id;
+    } else {
+      qq = e.message.filter(item => item.type == 'at')?.map(item => item?.qq);
+    }
+    try {
+      let json = await fetch(`http://api.cunyx.cn/Yunzai-Bot/cpl.php?qq=${data.qq}&token=${data.api}`);
+      json = await json.json();
+      var text = json;
+      let msg = [
+        segment.at(qq),
+        "\n",
+        text.msg
+      ];
+      e.reply(msg);
+    } catch (err) {
+      e.reply('api请求错误');
+    }
+  }
+}
