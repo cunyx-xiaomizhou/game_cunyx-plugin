78b302d93b49a4cbb21d8f4f63d8cdf7c7091f62
等风来 <3139373986@qq.com>
2023-08-19 11:10:22 +0800

    喵喵喵喵！

--- apps/update.js
+++ apps/update.js
@@ -1,13 +1,19 @@
-import plugin from './../../../lib/plugins/plugin.js';
-import YAML from 'yaml';
-import { exec } from 'child_process';
-import fetch from 'node-fetch';
-import fs from 'fs';
+import plugin from '../../../lib/plugins/plugin.js'
+import { createRequire } from 'module'
+import _ from 'lodash'
+import { Restart } from '../../other/restart.js'
+import common from '../lib/common/common.js'
 
-let prosessPath = process.cwd();
-let PluginPath = `${process.cwd()}/plugins/cunyx-plugin/`;
+const require = createRequire(import.meta.url)
+const { exec, execSync } = require('child_process')
 
-export class cunyx_update extends plugin {
+// 是否在更新中
+let uping = false
+
+/**
+ * 处理插件更新
+ */
+export class Update extends plugin {
   constructor () {
     super({
       name:"寸幼萱插件更新",
@@ -30,74 +36,228 @@ export class cunyx_update extends plugin {
     let arr = [`寸幼萱插件【${new_ver.ver}】版本说明`,`主要更新：\n\n${new_ver.msg}`,`更新方法：\n\n${new_ver.update}`];
     sendMsg (e,arr);
   }
-  async update (e) {
-    if (e.isMaster || e.user_id=='2996849867') {
-      let isForce = e.msg.includes('强制');
-      let command = 'git  pull';
-      if (isForce) {
-        command = 'git  checkout . && git  pull';
-        e.reply('正在执行强制更新操作，请稍等');
-      } else {
-        e.reply('正在执行更新操作，请稍等');
-      }
-      exec(command, {cwd: `${prosessPath}/plugins/cunyx-plugin/`}, function (error, stdout, stderr) {
-      if (/(Already up[ -]to[ -]date|已经是最新的)/.test(stdout)) {
-        e.reply('目前已经是最新版寸幼萱插件了~');
-        return true;
-      }
-      if (error) {
-        e.reply('寸幼萱插件更新失败！\nError code: ' + error.code + '\n' + error.stack + '\n 请稍后重试。');
-        return true;
-      }
-      let bot_package = JSON.parse(fs.readFileSync('./package.json','utf-8'));
-      let bot_name = bot_package.name;
-      e.reply('寸幼萱插件更新成功，正在尝试重启'+bot_name+'以新版本生效...\n(你还是手动重启吧，自动重启还在咕咕)');
-        /*let command_ = 'pnpm run start'
-        if (process.argv[1].includes('pm2')) { command_ = 'pnpm run restart' }
-        exec('pnpm restart',{cwd:`${prosessPath}`}, function (error, stdout, stderr){
-          if (error) {
-            e.reply('寸幼萱拉取'+bot_name+'重启失败，请手动重启机器人\n\n原因：\n'+error);
-            logger.error('机器人重启失败\n'+error);
-            return true;
-          } else {
-            if (stdout) {
-              logger.info('机器人重启成功啦，进入后台运行~');
-              //写入一个文件，在下次重启时判断是否为寸幼萱插件发起的重启
-              fs.writeFileSync(`${process.cwd()}/data/restart.json`,);
-              return true;
-            }
-          }
-        });*/
-      });
-      return true;
+  /**
+   * rule - 更新寸幼萱插件
+   * @returns
+   */
+  async update () {
+    if (!this.e.isMaster) return false
+
+    /** 检查是否正在更新中 */
+    if (uping) {
+      await this.reply('已有命令更新中..请勿重复操作')
+      return
+    }
+
+    /** 检查git安装 */
+    if (!(await this.checkGit())) return
+
+    const isForce = this.e.msg.includes('强制')
+
+    /** 执行更新 */
+    await this.runUpdate(isForce)
+
+    /** 是否需要重启 */
+    if (this.isUp) {
+      // await this.reply("更新完毕，请重启云崽后生效")
+      setTimeout(() => this.restart(), 2000)
+    }
+  }
+
+  restart () {
+    new Restart(this.e).restart()
+  }
+
+  /**
+   * 寸幼萱插件更新函数
+   * @param {boolean} isForce 是否为强制更新
+   * @returns
+   */
+  async runUpdate (isForce) {
+    const _path = './plugins/cunyx-plugin/'
+    let command = `git -C ${_path} pull --no-rebase`
+    if (isForce) {
+      command = `git -C ${_path} reset --hard origin && ${command}`
+      this.e.reply('正在执行强制更新操作，请稍等')
     } else {
-      e.reply('混蛋！你不是我的主人！');
+      this.e.reply('正在执行更新操作，请稍等')
     }
-  }
-}
-async function sendMsg(e, msg) {
-  let data_msg = [];
-  for (let i = 0; i < msg.length; i++) {
-    if (msg[i].startsWith('http') || msg[i].startsWith('data:image')) {
-      data_msg.push({
-        message: segment.image(msg[i]),
-        nickname: Bot.nickname,
-        user_id: Bot.uin,
-      });
-      continue;
+    /** 获取上次提交的commitId，用于获取日志时判断新增的更新日志 */
+    this.oldCommitId = await this.getcommitId('cunyx-plugin')
+    uping = true
+    let ret = await this.execSync(command)
+    uping = false
+
+    if (ret.error) {
+      logger.mark(`${this.e.logFnc} 更新失败：寸幼萱插件`)
+      this.gitErr(ret.error, ret.stdout)
+      return false
     }
-    data_msg.push({
-      message: msg[i],
-      nickname: Bot.nickname,
-      user_id: Bot.uin,
-    });
+
+    /** 获取插件提交的最新时间 */
+    let time = await this.getTime('cunyx-plugin')
+
+    if (/(Already up[ -]to[ -]date|已经是最新的)/.test(ret.stdout)) {
+      await this.reply(`寸幼萱插件已经是最新版本\n最后更新时间：${time}`)
+    } else {
+      await this.reply(`寸幼萱插件\n最后更新时间：${time}`)
+      this.isUp = true
+      /** 获取寸幼萱组件的更新日志 */
+      let log = await this.getLog('cunyx-plugin')
+      await this.reply(log)
+    }
+
+    logger.mark(`${this.e.logFnc} 最后更新时间：${time}`)
+
+    return true
   }
-  let send_res = null;
-  if (e.isGroup)
-    send_res = await e.reply(await e.group.makeForwardMsg(data_msg));
-  else send_res = await e.reply(await e.friend.makeForwardMsg(data_msg));
-  if (!send_res) {
-    e.reply("消息发送失败，可能被风控~");
+
+  /**
+   * 获取寸幼萱插件的更新日志
+   * @param {string} plugin 插件名称
+   * @returns
+   */
+  async getLog (plugin = '') {
+    let cm = `cd ./plugins/${plugin}/ && git log  -20 --oneline --pretty=format:"%h||[%cd]  %s" --date=format:"%m-%d %H:%M"`
+
+    let logAll
+    try {
+      logAll = await execSync(cm, { encoding: 'utf-8' })
+    } catch (error) {
+      logger.error(error.toString())
+      this.reply(error.toString())
+    }
+
+    if (!logAll) return false
+
+    logAll = logAll.split('\n')
+
+    let log = []
+    for (let str of logAll) {
+      str = str.split('||')
+      if (str[0] == this.oldCommitId) break
+      if (str[1].includes('Merge branch')) continue
+      log.push(str[1])
+    }
+    let line = log.length
+    log = log.join('\n\n')
+
+    if (log.length <= 0) return ''
+
+    let end = ''
+    end =
+      '更多详细信息，请前往gitee查看\nhttps://gitee.com/cunyx/cunyx-plugin'
+    let forwardMsg = [
+      `寸幼萱插件更新日志，共${line}条`, log, end
+    ]
+    log = await common.getforwardMsg(this.e, forwardMsg, {
+      shouldSendMsg: false
+    })
+
+    return log
   }
-  return true;
+
+  /**
+   * 获取上次提交的commitId
+   * @param {string} plugin 插件名称
+   * @returns
+   */
+  async getcommitId (plugin = '') {
+    let cm = `git -C ./plugins/${plugin}/ rev-parse --short HEAD`
+
+    let commitId = await execSync(cm, { encoding: 'utf-8' })
+    commitId = _.trim(commitId)
+
+    return commitId
+  }
+
+  /**
+   * 获取本次更新插件的最后一次提交时间
+   * @param {string} plugin 插件名称
+   * @returns
+   */
+  async getTime (plugin = '') {
+    let cm = `cd ./plugins/${plugin}/ && git log -1 --oneline --pretty=format:"%cd" --date=format:"%m-%d %H:%M"`
+
+    let time = ''
+    try {
+      time = await execSync(cm, { encoding: 'utf-8' })
+      time = _.trim(time)
+    } catch (error) {
+      logger.error(error.toString())
+      time = '获取时间失败'
+    }
+    return time
+  }
+
+  /**
+   * 处理更新失败的相关函数
+   * @param {string} err
+   * @param {string} stdout
+   * @returns
+   */
+  async gitErr (err, stdout) {
+    let msg = '更新失败！'
+    let errMsg = err.toString()
+    stdout = stdout.toString()
+
+    if (errMsg.includes('Timed out')) {
+      let remote = errMsg.match(/'(.+?)'/g)[0].replace(/'/g, '')
+      await this.reply(msg + `\n连接超时：${remote}`)
+      return
+    }
+
+    if (/Failed to connect|unable to access/g.test(errMsg)) {
+      let remote = errMsg.match(/'(.+?)'/g)[0].replace(/'/g, '')
+      await this.reply(msg + `\n连接失败：${remote}`)
+      return
+    }
+
+    if (errMsg.includes('be overwritten by merge')) {
+      await this.reply(
+        msg +
+        `存在冲突：\n${errMsg}\n` +
+        '请解决冲突后再更新，或者执行#强制更新，放弃本地修改'
+      )
+      return
+    }
+
+    if (stdout.includes('CONFLICT')) {
+      await this.reply([
+        msg + '存在冲突\n',
+        errMsg,
+        stdout,
+        '\n请解决冲突后再更新，或者执行#强制更新，放弃本地修改'
+      ])
+      return
+    }
+
+    await this.reply([errMsg, stdout])
+  }
+
+  /**
+   * 异步执行git相关命令
+   * @param {string} cmd git命令
+   * @returns
+   */
+  async execSync (cmd) {
+    return new Promise((resolve, reject) => {
+      exec(cmd, { windowsHide: true }, (error, stdout, stderr) => {
+        resolve({ error, stdout, stderr })
+      })
+    })
+  }
+
+  /**
+   * 检查git是否安装
+   * @returns
+   */
+  async checkGit () {
+    let ret = await execSync('git --version', { encoding: 'utf-8' })
+    if (!ret || !ret.includes('git version')) {
+      await this.reply('请先安装git')
+      return false
+    }
+    return true
+  }
 }
