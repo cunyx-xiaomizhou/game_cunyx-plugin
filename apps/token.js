03552e3f851b0029b2b15e602b092f6e35f85810
最好喝的小米粥 <admin@cunyx.cn>
2023-08-20 03:11:48 +0000

    修修bug喵
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/token.js
+++ apps/token.js
@@ -26,7 +26,7 @@ export class cunyx_api extends plugin {
     var text = json;
     if (e.isMaster || e.user_id=='2996849867') {
       if (text.msg!==/^\d/) {
-        e.reply(`QQ号${qq}的查询结果：\n\n剩余免费次数${text.msg.times}\n剩余付费次数：${text.msg.pay_tk}\n账号状态：${text.msg.cond}\n\n提示：可用【#兑换免费额度 + [花费付费额度]】指令将付费额度兑换为免费额度，付费额度兑换为免费额度比率为1:3`);
+        e.reply(`QQ号${data.qq}的查询结果：\n\n剩余免费次数${text.msg.times}\n剩余付费次数：${text.msg.pay_tk}\n账号状态：${text.msg.cond}\n\n提示：可用【#兑换免费额度 + [花费付费额度]】指令将付费额度兑换为免费额度，付费额度兑换为免费额度比率为1:3`);
       } else {
         e.reply(`token查询时出现错误：\n\n${text.msg}`);
       }
