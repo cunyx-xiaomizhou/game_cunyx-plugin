c4c73418747134573c8cddc0f69c3a0754943856
最好喝的小米粥 <admin@cunyx.cn>
2023-09-16 12:36:32 +0000

    完成初始化框架(没有写入) 下一个就是随机数函数了，因为那个没写完，这个功能不完美
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- model/start.js
+++ model/start.js
@@ -1,6 +1,11 @@
 import fs from 'fs';
 import YAML from 'yaml';
 export function start (e) {
-  fs.writeFileSync();
-  e.reply("nalanala");
+  let system_start = YAML.parse(fs.readFileSync('./plugins/impart_cunyx-plugin/config/impart_start.yaml','utf-8'));
+  try {
+    fs.readFileSync(process.cwd() + "/plugins/impart_cunyx-plugin/data/" + e.group_id + ".json");
+    e.reply();
+  } catch (err) {
+    e.reply();
+  }
 }
