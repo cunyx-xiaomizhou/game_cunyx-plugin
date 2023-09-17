c62af58428a08e111f281e74d188833a566c745e
最好喝的小米粥 <admin@cunyx.cn>
2023-09-17 04:03:28 +0000

    读取控制面板值的函数
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

---
+++ model/GetYamlValue.js
@@ -0,0 +1,12 @@
+import YAML from 'yaml';
+import fs from 'fs';
+export function GetYamlValue (e,FileName,Name) {
+  let data = YAML.parse(fs.readFileSync('./plugins/impart_cunyx-plugin/config/' + FileName +'.yaml','utf-8'));
+  try {
+    let value = data[e.group_id].Name;
+    return value;
+  } catch (err) {
+    let value = data.def.Name;
+    return value;
+  }
+}
