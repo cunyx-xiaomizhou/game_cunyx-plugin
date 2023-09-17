11f4a24d2c9e6bdb2a49f3b8c1875a55a28af5f5
最好喝的小米粥 <admin@cunyx.cn>
2023-09-17 04:08:45 +0000

    取数组中随机一个值函数
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

---
+++ model/RandomArrayValueIndex.js
@@ -0,0 +1,9 @@
+import random from 'random.js';
+export function RandomArrayValueIndex (array) {
+  //计算索引
+  let randomIndex = random(0,array.length - 1);
+  //取得值
+  let randomElement = array[randomIndex];
+  //返回值
+  return randomElement;
+}
