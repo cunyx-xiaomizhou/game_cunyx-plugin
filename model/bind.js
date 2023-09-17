c55beb3ad399931f98a497df3c003f50704af641
最好喝的小米粥 <admin@cunyx.cn>
2023-09-17 07:30:45 +0000

    适配自定义文案
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- model/bind.js
+++ model/bind.js
@@ -1,4 +1,6 @@
 import fs from 'fs';
+import { GetYamlValue } from 'GrtYamlValue.js';
+import { RandomArrayValueIndex } from 'RandomArrayValueIndex.js';
 export function bind (e,qq_id,qun_id) {
   let JsonText = fs.readFileSync('./plugins/impart_cunyx-plugin/data/bind.json');
   let Json = JSON.parse(JsonText);
@@ -6,6 +8,9 @@ export function bind (e,qq_id,qun_id) {
   let NewJson = JSON.stringify(Json);
   fs.writeFile('./plugins/impart_cunyx-plugin/data/bind.json', NewJson, (err) => {
     if (err) throw err;
-    e.reply("绑定成功！",true);
+    let wenan_array = GetYamlValue(e,"bind","language");
+    let wenan = RandomArrayValueIndex(wenan_array);
+    let msg = e.msg.replace(/{qq_id}/,qq_id).replace(/{qun_id}/,qun_id)/
+    e.reply(msg,true);
   });
 }
