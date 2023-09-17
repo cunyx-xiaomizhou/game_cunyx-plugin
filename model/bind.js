32969b04188583970aa876a4599448695ea97e5d
最好喝的小米粥 <admin@cunyx.cn>
2023-09-17 06:55:12 +0000

    传入用户事件参数，用于发消息
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- model/bind.js
+++ model/bind.js
@@ -1,11 +1,11 @@
 import fs from 'fs';
-export function bind (qq_id,qun_id) {
+export function bind (e,qq_id,qun_id) {
   let JsonText = fs.readFileSync('./plugins/impart_cunyx-plugin/data/bind.json');
   let Json = JSON.parse(JsonText);
   Json[qq_id] = qun_id;
   let NewJson = JSON.stringify(Json);
   fs.writeFileSync('./plugins/impart_cunyx-plugin/data/bind.json', NewJson, function (err) {
     if (err) throw err;
-    e.reply("绑定成功！");
+    e.reply("绑定成功！",true);
   });
 }
