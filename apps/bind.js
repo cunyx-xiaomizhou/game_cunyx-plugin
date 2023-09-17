fea35f8f58b2a2422facab0bdcf043b99c5940cc
最好喝的小米粥 <admin@cunyx.cn>
2023-09-17 06:21:45 +0000

    测试白名单内用户信息获取
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/bind.js
+++ apps/bind.js
@@ -1,6 +1,9 @@
 
 import plugin from './../../../lib/plugins/plugin.js';
 import { GetYamlValue } from './../model/GetYamlValue.js';
+
+let qun_id;
+
 export class cunyx_impart_bind extends plugin {
   constructor () {
     super({
@@ -9,14 +12,25 @@ export class cunyx_impart_bind extends plugin {
       event:"message",
       priority:1,/*优先级*/
       rule:[
-        {reg:"^#?绑定(.*)",fnc:"bind"},
+        {reg:"^#?(淫|银|阴)(趴|扒|啪|爬)绑定(.*)",fnc:"bind"},
         {reg:"^#?查看(当前)绑定$",fnc:"seekbind"}
       ]
     });
   }
   async bind (e) {
-    if (GetYamlValue(e,"bind","cond")) {
-      e.reply("函数可用");
+    //优先判断用户是否在白名单内
+    if (GetYamlValue(e,"bind","white").includes(e.user_id)) {
+      //用户处于白名单
+      if (e.msg.includes("本")||e.msg.includes("此")) {
+        let qun_id = e.group_id;
+        qun_id = qun_id;
+      } else {
+        let qun_id = e.mag.replace(/(淫|银|阴)(趴|扒|啪|爬)绑定|#/g,"");
+        qun_id = qun_id;
+      }
+      e.reply("【测试】"+qun_id);
+    } else if (GetYamlValue(e,"bind","cond") == true) {
+      e.reply("【测试】未在白名单")
     }
   }
 }
