129a99f0e992996c744b6783df0908304305489c
最好喝的小米粥 <admin@cunyx.cn>
2023-09-17 05:46:02 +0000

    修复方括号取值语法错误
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- model/GetYamlValue.js
+++ model/GetYamlValue.js
@@ -3,10 +3,10 @@ import fs from 'fs';
 export function GetYamlValue (e,FileName,Name) {
   let data = YAML.parse(fs.readFileSync('./plugins/impart_cunyx-plugin/config/' + FileName +'.yaml','utf-8'));
   try {
-    let value = data[e.group_id].Name;
+    let value = data[e.group_id][Name];
     return value;
   } catch (err) {
-    let value = data.def.Name;
+    let value = data.def[Name];
     return value;
   }
 }
