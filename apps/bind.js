f8f39dfc59aa7eadee0fbc12a6e9f8bf5ccb1003
最好喝的小米粥 <admin@cunyx.cn>
2023-09-19 15:23:04 +0000

    完成绑定功能
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/bind.js
+++ apps/bind.js
@@ -1,6 +1,7 @@
 import { bind } from './../model/bind.js';
 import plugin from './../../../lib/plugins/plugin.js';
 import { GetYamlValue } from './../model/GetYamlValue.js';
+import { GetYamlArrayRandomValue } from './../model/GetYamlArrayRandomValue.js';
 
 let qun_id;
 
@@ -23,7 +24,8 @@ export class cunyx_impart_bind extends plugin {
       //用户处于白名单
       if (e.msg.includes("本")||e.msg.includes("此")) {
         if (!e.group_id) {
-          e.reply("私聊仅支持使用群号绑定",true);
+          
+          e.reply(GetYamlArrayRandomValue(e,"bind","FriendBindThis"),true);
           return true;
         }
         qun_id = e.group_id;
@@ -36,13 +38,13 @@ export class cunyx_impart_bind extends plugin {
       //不处于白名单但绑定功能开启
       if (GetYamlValue(e,"bind","blackQQ").includes(e.user_id)) {
         //用户在黑名单内
-        e.reply("你被禁止使用绑定功能",true);
+        e.reply(GetYamlArrayRandomValue(e,"bind","BlackUserBind"),true);
         return false;
       }
       if (!GetYamlValue(e,"bind","refuse").includes(qun_id)) {
         if (e.msg.includes("本")||e.msg.includes("此")) {
           if (!e.group_id) {
-            e.reply("私聊仅支持使用群号绑定",true);
+            e.reply(GetYamlArrayRandomValue(e,"bind","FriendBindThis"),true);
             return false;
           }
           qun_id = e.group_id;
@@ -52,12 +54,12 @@ export class cunyx_impart_bind extends plugin {
         bind(e,e.user_id,qun_id);
       } else {
         //群聊属于黑名单，无法绑定
-        e.reply("该群聊已被禁止绑定",true);
+        e.reply(GetYamlArrayRandomValue(e,"bind","BlackGroupBind"),true);
         return false;
       }
     } else {
       //功能关闭，且不存在白名单
-      e.reply("暂未开启绑定功能",true);
+      e.reply(GetYamlArrayRandomValue(e,"bind","BindClose"),true);
       return false;
     }
   }
@@ -66,9 +68,10 @@ export class cunyx_impart_bind extends plugin {
       let TextJson = fs.readFileSync('./plugins/impart_cunyx-plugin/data/bind.json');
       let Json = JSON.parse(TextJson);
       let group_id = Json[e.user_id];
-      e.reply("你的绑定群聊为："+group_id,true);
+      let Msg = GetYamlArrayRandomValue(e,"bind","Seek").replace(/{BindQunId}/,group_id);
+      e.reply(Msg,true);
     } catch (err) {
-      e.reply("你还没有绑定过群聊",true);
+      e.reply(GetYamlArrayRandomValue(e,"bind","NoneBind"),true);
     }
   }
 }
