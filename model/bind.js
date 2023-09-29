423ac537958de73edace2aacce00692a09be69a0
最好喝的小米粥 <admin@cunyx.cn>
2023-09-29 10:49:57 +0000

    更新了绑定模型
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- model/bind.js
+++ model/bind.js
@@ -1,12 +1,18 @@
 import fs from 'fs';
 import { GetYamlValue } from './GetYamlValue.js';
 import { RandomArrayValueIndex } from './RandomArrayValueIndex.js';
+let dir;
 export function bind (e,qq_id,qun_id) {
   let JsonText = fs.readFileSync('./plugins/impart_cunyx-plugin/data/bind.json');
   let Json = JSON.parse(JsonText);
   Json[qq_id] = qun_id;
   let NewJson = JSON.stringify(Json);
-  fs.writeFile('./plugins/impart_cunyx-plugin/data/bind.json', NewJson, (err) => {
+  if (GetYamlValue(e,"data","IsBind")==true) {
+    dir = Bot.uin;
+  } else {
+    dir = "all";
+  }
+  fs.writeFile('./plugins/impart_cunyx-plugin/data/' + dir + 'bind.json', NewJson, (err) => {
     if (err) throw err;
     let wenan_array = GetYamlValue(e,"bind","language");
     let wenan = RandomArrayValueIndex(wenan_array);
