e90fda51322ff1eff620d9f2f424ed9e9a802ae5
最好喝的小米粥 <admin@cunyx.cn>
2023-09-29 11:56:18 +0000

    学会不眼瞎
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- model/GetYamlArrayRandomValue.js
+++ model/GetYamlArrayRandomValue.js
@@ -1,6 +1,6 @@
 import { GetYamlValue } from './GetYamlValue.js';
 import { RandomArrayValueIndex } from './RandomArrayValueIndex.js';
-export function GetYamlRrrayRandomValue (FileName, Name) {
+export function GetYamlArrayRandomValue (e, FileName, Name) {
   let Array = GetYamlValue(e,FileName,Name);
   let Msg = RandomArrayValueIndex(Array);
   return Msg;
