2fed7e9a99f0c61ad4b6427f0e236702dd0e88e1
最好喝的小米粥 <admin@cunyx.cn>
2023-09-29 13:12:27 +0000

    写个框架叭，睡觉了，大家晚安
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

---
+++ apps/daoguanzi.js
@@ -0,0 +1,10 @@
+import plugin from './../../../lib/plugins/plugin.js';
+import { GetYamlValue } from './../model/GetYamlValue.js';
+export class cunyx_impart_bind extends plugin {
+  constructor () {
+    super({
+      name:"寸幼萱淫趴:导管子",
+      dsc:"让用户开心的去导管子吧！",
+      event:"message",
+      priority:1,/*优先级*/
+      rule:[
