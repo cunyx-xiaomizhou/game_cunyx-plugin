1efcf4a51eba34009068d33391a7e5c7ba170aa2
最好喝的小米粥 <admin@cunyx.cn>
2023-09-30 08:49:43 +0000

    更新控制台打印日志
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- model/RandomArrayValueIndex.js
+++ model/RandomArrayValueIndex.js
@@ -1,8 +1,11 @@
 import { random } from './random.js';
 export function RandomArrayValueIndex (array) {
   //计算索引
-  let randomIndex = random(0,array.length - 1);
-  Bot.logger.mark(randomIndex);
+  let long = array.length;
+  let randomIndex = random(0,long - 1);
+  Bot.logger.info("[寸幼萱淫趴][model/RandomArrayValueIndex]随机数组值请求成功！")
+  Bot.logger.mark("随机的数组值的索引是："+randomIndex);
+  Bot.logger.info("数组长度："+long);
   //取得值
   let randomElement = array[randomIndex];
   //返回值
