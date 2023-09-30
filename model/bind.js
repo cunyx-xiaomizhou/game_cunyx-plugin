f739d7da3727967cd662ed64d6953a9f430865a6
最好喝的小米粥 <admin@cunyx.cn>
2023-09-30 05:49:04 +0000

    更新一下绑定函数，适配第一次初始化
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- model/bind.js
+++ model/bind.js
@@ -8,15 +8,19 @@ export function bind (e,qq_id,qun_id) {
   } else {
     dir = Bot.uin;
   }
-  let JsonText = fs.readFileSync('./plugins/impart_cunyx-plugin/data/'+dir+'/bind.json');
-  let Json = JSON.parse(JsonText);
-  Json[qq_id] = qun_id;
-  let NewJson = JSON.stringify(Json);
-  fs.writeFile('./plugins/impart_cunyx-plugin/data/' + dir + '/bind.json', NewJson, (err) => {
-    if (err) throw err;
-    let wenan_array = GetYamlValue(e,"bind","language");
-    let wenan = RandomArrayValueIndex(wenan_array);
-    let msg = wenan.replace(/{qq_id}/,qq_id).replace(/{qun_id}/,qun_id);
-    e.reply(msg,true);
-  });
+  try {
+    let JsonText = fs.readFileSync('./plugins/impart_cunyx-plugin/data/'+dir+'/bind.json');
+    let Json = JSON.parse(JsonText);
+    Json[qq_id] = qun_id;
+    let NewJson = JSON.stringify(Json);
+    fs.writeFile('./plugins/impart_cunyx-plugin/data/' + dir + '/bind.json', NewJson, (err) => {
+      if (err) throw err;
+      let wenan_array = GetYamlValue(e,"bind","language");
+      let wenan = RandomArrayValueIndex(wenan_array);
+      let msg = wenan.replace(/{qq_id}/,qq_id).replace(/{qun_id}/,qun_id);
+      e.reply(msg,true);
+    });
+  } catch (err) {
+    start(e,"IsBind","data");
+  }
 }
