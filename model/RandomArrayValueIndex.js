9e68bbd3d800ac58a881fc45e7a76bfd47355cc8
最好喝的小米粥 <admin@cunyx.cn>
2023-09-30 06:26:06 +0000

    报错了，测试一下索引
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- model/RandomArrayValueIndex.js
+++ model/RandomArrayValueIndex.js
@@ -2,6 +2,7 @@ import { random } from './random.js';
 export function RandomArrayValueIndex (array) {
   //计算索引
   let randomIndex = random(0,array.length - 1);
+  e.reply(randomIndex);
   //取得值
   let randomElement = array[randomIndex];
   //返回值
