a4bf3bb1042c42234180335144d6a362cd563efd
最好喝的小米粥 <admin@cunyx.cn>
2023-09-30 08:03:40 +0000

    增加了测试命令
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- model/RandomArrayValueIndex.js
+++ model/RandomArrayValueIndex.js
@@ -2,6 +2,7 @@ import { random } from './random.js';
 export function RandomArrayValueIndex (array) {
   //计算索引
   let randomIndex = random(0,array.length - 1);
+  Bot.logger.mark(randomIndex);
   //取得值
   let randomElement = array[randomIndex];
   //返回值
