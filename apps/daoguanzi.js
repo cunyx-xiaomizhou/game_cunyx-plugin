f229465229f8840c0ce26ead53d4a4e2d73b4040
最好喝的小米粥 <admin@cunyx.cn>
2023-09-30 01:28:20 +0000

    感觉缺点什么
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/daoguanzi.js
+++ apps/daoguanzi.js
@@ -1,10 +1,20 @@
 import plugin from './../../../lib/plugins/plugin.js';
 import { GetYamlValue } from './../model/GetYamlValue.js';
-export class cunyx_impart_bind extends plugin {
+export class plugin_name extends plugin {
   constructor () {
     super({
       name:"寸幼萱淫趴:导管子",
-      dsc:"让用户开心的去导管子吧！",
+      dsc:"让用户开心的导管子吧！",
       event:"message",
       priority:1,/*优先级*/
       rule:[
+        {reg:"^#?"+GetYamlValue(e,"daoguanzi","reg")+"$",fnc:"dgz"}
+      ]
+    });
+  }
+  async daoguanzi (e) {
+    /*
+      需要一个判断是否有数据的函数，等我，我先去写
+    */
+  }
+}
