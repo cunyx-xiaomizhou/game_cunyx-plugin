f85eadc5a3fd5f7238c58be0d499d8dbe6a0c4c2
最好喝的小米粥 <admin@cunyx.cn>
2023-09-30 12:05:02 +0000

    好累

--- apps/daoguanzi.js
+++ apps/daoguanzi.js
@@ -4,6 +4,8 @@ import { GetBindQun } from './../model/GetBindQun.js';
 import plugin from './../../../lib/plugins/plugin.js';
 import { GetYamlValue } from './../model/GetYamlValue.js';
 import { GetYamlArrayRandomValue } from './../model/GetYamlArrayRandomValue.js';
+let BeQQ;
+let qun_id;
 export class CunyxImpart_daoguanzi extends plugin {
   constructor () {
     super({
@@ -23,14 +25,25 @@ export class CunyxImpart_daoguanzi extends plugin {
         let Msg = GetYamlArrayRandomValue(e,'daoguanzi','NotBind');
         e.reply(Msg,true);
         return false;
+      } else {
+        qun_id = qun_id;
       }
     } else {
-      qun_id = e.user_id;
+      qun_id = qun_id;
     }
-    start(e,IsPublic,qun_id);
-    if (GetYamlValue(e,"daoguanzi","cond")==false) {
-      e.reply("本群淫趴已被关闭，快去叫我的主人开启吧！",true);
-      return false;
+    //提取用户QQ号
+    let reg = new RegExp(GetYamlValue(e,'daoguanzi','reg'),"g");
+    let BeQQ = e.msg.replace(reg,"").trim();
+    if (/^\d{5,10}$/.test(BeQQ)==false) {
+      //QQ号格式不正确就从消息中强制提取
+      BeQQ = e.message.filter(item => item.type == 'at')?.map(item => item?.qq);
+    } else {
+      BeQQ = BeQQ;
+    }
+    if (GetYamlValue(e,'daoguanzi','cond')!==true) {
+      start(e,IsPublic,qun_id);
+    } else {
+      //淫趴被关闭
     }
   }
 }
