d451f0dccbcf1531eb5d675d320f25c3caeefa9c
最好喝的小米粥 <admin@cunyx.cn>
2023-09-30 05:30:21 +0000

    不知道是谁加了个true，肯定不是我
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- model/start.js
+++ model/start.js
@@ -19,7 +19,7 @@ export function start (e,IsData,FileName) {
       fs.writeFile('./plugins/impart_cunyx-plugin/data/'+dir+'/'+FileName+'.json',JSON.stringify("{}"),(err) => {
         if (err) throw err;
         let Msg = GetYamlArrayRandomValue(e,"system","start").replace(/{_path_}/,"data/"+dir+"/"+FileName+".json");
-        Bot.logger.info("[寸幼萱淫趴][model/start]"+Msg,true);
+        Bot.logger.info("[寸幼萱淫趴][model/start]"+Msg);
         e.reply(Msg,true);
       });
     }
