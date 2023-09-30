a14cbeea91e6144814424b300880d2bdbe69850d
最好喝的小米粥 <admin@cunyx.cn>
2023-09-30 12:22:30 +0000

    睡觉喽

--- model/GetBindQun.js
+++ model/GetBindQun.js
@@ -2,17 +2,22 @@ import fs from 'fs';
 import { GetYamlValue } from './GetYamlValue.js';
 let dir;
 export function GetBindQun (e) {
+  Bot.logger.mark("[寸幼萱淫趴][model/GetBindQun]正在请求绑定QQ号......");
   if (GetYamlValue(e,"data","IsBind")==true) {
     dir = Bot.uin;
+    Bot.logger.mark("在局部数据查找中...");
   } else {
     dir = "all";
+    Bot.logger.mark("在全局数据查找中...");
   }
   try {
     let TextJson = fs.readFileSync('./plugins/impart_cunyx-plugin/data/'+dir+'/bind.json');
     let Json = JSON.parse(TextJson);
     let qun = Json[e.user_id];
+    Bot.logger.info("查询成功，返回"+qun);
     return qun;
   } catch (err) {
+    Bot.logger.error("查找失败，返回null");
     return null;
   }
 }
--- model/RandomArrayValueIndex.js
+++ model/RandomArrayValueIndex.js
@@ -5,7 +5,7 @@ export function RandomArrayValueIndex (array) {
   let randomIndex = random(0,long - 1);
   Bot.logger.info("[寸幼萱淫趴][model/RandomArrayValueIndex]随机数组值请求成功！")
   Bot.logger.mark("随机的数组值的索引是："+randomIndex);
-  Bot.logger.info("数组长度："+long);
+  Bot.logger.mark("数组长度："+long);
   //取得值
   let randomElement = array[randomIndex];
   //返回值
