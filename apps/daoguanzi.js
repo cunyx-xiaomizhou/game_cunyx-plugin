a5dedce7a3fdbfec8c617a7d9e475b3b13704042
最好喝的小米粥 <admin@cunyx.cn>
2023-10-01 06:51:24 +0000

    就差数据写入和文案调取了，加油！
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/daoguanzi.js
+++ apps/daoguanzi.js
@@ -2,10 +2,12 @@ import fs from 'fs';
 import { start } from './../model/start.js';
 import { GetBindQun } from './../model/GetBindQun.js';
 import plugin from './../../../lib/plugins/plugin.js';
+import { LooseNumber } from './../model/LooseNumber.js';
 import { GetYamlValue } from './../model/GetYamlValue.js';
 import { GetYamlArrayRandomValue } from './../model/GetYamlArrayRandomValue.js';
 let BeQQ;
 let qun_id;
+let Number;
 export class CunyxImpart_daoguanzi extends plugin {
   constructor () {
     super({
@@ -31,6 +33,17 @@ export class CunyxImpart_daoguanzi extends plugin {
     } else {
       qun_id = e.group_id;
     }
+    //获取控制面板需要的值
+    let low = GetYamlValue(e,"daoguanzi","add_low",qun_id);
+    let heigh = GetYamlValue(e,"daoguanzi","add_heigh",qun_id);
+    let cd = GetYamlValue(e,"daoguanzi","cd",qun_id);
+    let Num = random(low,height);
+    //计算增长值
+    if (Num==height) {
+      Number = Num;
+    } else {
+      Number = Num + LooseNumber(random(0,999),3)/1000;
+    }
     //提取用户QQ号
     let reg = new RegExp(GetYamlValue(e,'daoguanzi','reg'),"g");
     let BeQQ = e.msg.replace(reg,"").trim();
@@ -44,6 +57,12 @@ export class CunyxImpart_daoguanzi extends plugin {
     }
     if (GetYamlValue(e,'daoguanzi','cond')==true) {
       start(e,"IsPublic",qun_id);
+      /**
+       * @这里打算写一个函数读取JSON数据
+       * @毕竟这个同步系统搞的我是心乱啊！
+       * @呜呜呜安慰安慰孩子发点电吧
+       */
+      //let OldJson = JSON.parse();
       if (e.group_id) {
         //群聊导管子
       } else {
