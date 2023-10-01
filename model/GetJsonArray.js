a35c4f5776b41f91f725c1da7476a1f39e43781b
最好喝的小米粥 <admin@cunyx.cn>
2023-10-01 00:42:58 +0000

    删除文件 model/GetJsonArray.js

--- model/GetJsonArray.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-export function GetJsonArray (path,name) {
-  let TextJson = fs.readFileSync(process.cwd()+'/plugins/impart_cunyx-plugin/'+name+'.json');
-  //字符串转json对象，咕咕/
-}
