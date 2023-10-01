d2bf6463f6b214611e6194484b8b092c9ec65ce3
最好喝的小米粥 <admin@cunyx.cn>
2023-10-01 00:41:40 +0000

    获取控制面板值函数
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- model/GetYamlValue.js
+++ model/GetYamlValue.js
@@ -1,17 +1,33 @@
 import YAML from 'yaml';
 import fs from 'fs';
 export function GetYamlValue (e,FileName,Name) {
+  Bot.logger.mark("[寸幼萱淫趴][model/GetYamlValue]开始调用获取控制面板值函数...");
+  Bot.logger.mark("目标文件名："+FileName+'.yaml')
   let data = YAML.parse(fs.readFileSync('./plugins/impart_cunyx-plugin/config/' + FileName +'.yaml','utf-8'));
   try {
+    Bot.logger.mark("尝试读取群单独配置....");
     let value = data[e.group_id][Name];
+    Bot.logger.info("读取群配置成功，返回"+value);
     return value;
   } catch (err) {
+    Bot.logger.error("读取群配置失败，尝试读取默认配置");
     try {
       let value = data.def[Name];
+      Bot.logger.info("默认值读取成功，返回"+value);
       return value;
     } catch (err) {
-      let value = data[Name];
-      return value;
+      try {
+        Bot.logger.error("默认值读取失败，正在使用常规读取方式...");
+        let value = data[Name];
+        Bot.logger.info("读取成功，返回"+value);
+        return value;
+      } catch (err) {
+        Bot.logger.error("读取失败！错误如下：");
+        Bot.logger.error(err);
+        e.reply("程序出错，报错已打印至控制台，请联系相关人员处理",true);
+        Bot.logger.error("返回false，程序终止运行");
+        return false;
+      }
     }
   }
 }
