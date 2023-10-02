b16a64aee4354911216d15c1d70fc681cd7da14d
最好喝的小米粥 <admin@cunyx.cn>
2023-10-02 07:55:29 +0000

    为啥返回空啊
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- model/PostTextJsonData.js
+++ model/PostTextJsonData.js
@@ -12,8 +12,8 @@ export function PostTextJsonData (e,qun_id,TextJson) {
     fs.writeFile("./plugins/impart_cunyx-plugin/data/"+dir+"/"+qun_id+".json",JSON.stringify(TextJson),(err) => {
       if (err) throw err;
       Bot.logger.info("提交成功，返回true");
-      return true;
     });
+    return true;
   } catch (err) {
     Bot.logger.error("提交失败，错误如下：");
     Bot.logger.error(err);
