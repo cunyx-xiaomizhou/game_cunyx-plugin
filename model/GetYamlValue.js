9fe69932c49cdc43ac7a2eacaf4268cccb2ab5e0
最好喝的小米粥 <admin@cunyx.cn>
2023-10-02 03:30:45 +0000

    手动抛出错误
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- model/GetYamlValue.js
+++ model/GetYamlValue.js
@@ -8,22 +8,26 @@ export function GetYamlValue (e,FileName,Name,qun_id) {
     Bot.logger.mark("尝试读取群单独配置....");
     if (qun_id) {
       let value = data[qun_id][Name];
+      if (value==undefined) throw undefined;
       Bot.logger.info("读取群配置成功，返回"+value);
       return value;
     }
     let value = data[e.group_id][Name];
+    if (value==undefined) throw undefined;
     Bot.logger.info("读取群配置成功，返回"+value);
     return value;
   } catch (err) {
     Bot.logger.mark("读取群配置失败，尝试读取默认配置");
     try {
       let value = data.def[Name];
+      if (value==undefined) throw undefined;
       Bot.logger.info("默认值读取成功，返回"+value);
       return value;
     } catch (err) {
       try {
         Bot.logger.mark("默认值读取失败，正在使用常规读取方式...");
         let value = data[Name];
+        if (value==undefined) throw undefined;
         Bot.logger.info("读取成功，返回"+value);
         return value;
       } catch (err) {
