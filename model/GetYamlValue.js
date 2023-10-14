64e2f482fe367ac344b8a7ddfa1504373d5d8a36
最好喝的小米粥 <admin@cunyx.cn>
2023-10-14 13:03:45 +0000

    偷个懒好吧
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- model/GetYamlValue.js
+++ model/GetYamlValue.js
@@ -1,38 +1,66 @@
 import YAML from 'yaml';
 import fs from 'fs';
-export function GetYamlValue (e,FileName,Name,qun_id) {
+
+export function GetYamlValue(e, FileName, Name, qun_id) {
   Bot.logger.mark("[寸幼萱淫趴][model/GetYamlValue]开始调用获取控制面板值函数...");
   Bot.logger.mark("目标文件名："+FileName+'.yaml；寻找键：'+Name);
   let data = YAML.parse(fs.readFileSync('./plugins/impart_cunyx-plugin/config/' + FileName +'.yaml','utf-8'));
+  
   try {
     Bot.logger.mark("尝试读取群单独配置....");
     if (qun_id) {
       let value = data[qun_id][Name];
-      if (value==undefined) throw undefined;
+      if (Array.isArray(value)) {
+        Bot.logger.info("读取群配置成功，返回"+JSON.stringify(value));
+        return value;
+      } else if (typeof value === 'string') {
+        let Value = value.replace(/{or}/g,"|");
+        Bot.logger.info("读取群配置成功，返回"+Value);
+        return Value;
+      } else {
+        throw new Error("无效的配置值");
+      }
+    }
+    
+    let value = data[e.group_id][Name];
+    if (Array.isArray(value)) {
+      Bot.logger.info("读取群配置成功，返回"+JSON.stringify(value));
+      return value;
+    } else if (typeof value === 'string') {
       let Value = value.replace(/{or}/g,"|");
       Bot.logger.info("读取群配置成功，返回"+Value);
       return Value;
+    } else {
+      throw new Error("无效的配置值");
     }
-    let value = data[e.group_id][Name];
-    if (value==undefined) throw undefined;
-    let Value = value.replace(/{or}/g,"|");
-    Bot.logger.info("读取群配置成功，返回"+Value);
-    return Value;
   } catch (err) {
     Bot.logger.mark("读取群配置失败，尝试读取默认配置");
     try {
       let value = data.def[Name];
-      if (value==undefined) throw undefined;
-      let Value = value.replace(/{or}/g,"|");
-      Bot.logger.info("默认值读取成功，返回"+Value);
-      return Value;
+      if (Array.isArray(value)) {
+        Bot.logger.info("默认值读取成功，返回"+JSON.stringify(value));
+        return value;
+      } else if (typeof value === 'string') {
+        let Value = value.replace(/{or}/g,"|");
+        Bot.logger.info("默认值读取成功，返回"+Value);
+        return Value;
+      } else {
+        throw new Error("无效的配置值");
+      }
     } catch (err) {
       try {
         Bot.logger.mark("默认值读取失败，正在使用常规读取方式...");
         let value = data[Name];
-        let Value = value.replace(/{or}/g,"|");
-        Bot.logger.info("读取成功，返回"+Value);
-        return Value;
+        if (Array.isArray(value)) {
+          Bot.logger.info("读取成功，返回"+JSON.stringify(value));
+          return value;
+        } else if (typeof value === 'string') {
+          let Value = value.replace(/{or}/g,"|");
+          Bot.logger.info("读取成功，返回"+Value);
+          return Value;
+        } else {
+          throw new Error("无效的配置值");
+        }
       } catch (err) {
         Bot.logger.error("读取失败！错误如下：");
         Bot.logger.error(err);
