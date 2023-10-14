2a8a7e3b667cafca3d6d5d3f87edd64b000d8397
最好喝的小米粥 <admin@cunyx.cn>
2023-10-14 09:01:58 +0000

    常用函数总集合
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

---
+++ model/index.js
@@ -0,0 +1,26 @@
+/**
+ * @最好喝的小米粥
+ * By 2023-10-14 16:
+ * http://afdian.net/a/woxmz
+ * 集合本文件夹下所有模型及函数
+ */
+import { start } from './start.js';
+import { random } from './random.js';
+import { GetDate } from './GetDate.js';
+import { GetBindQun } from './GetBindQun.js';
+import { LooseNumber } from './LooseNumber.js';
+import { GetYamlValue } from './GetyamlValue.js';
+import { GetTextJsonData } from './GetTextJsonData.js';
+import { PostTextJsonData } from './PostTextJsonData.js';
+import { GetYamlArrayRandomValue } from './GetYamlArrayRandomValue.js';
+export { 
+    start, 
+    random, 
+    GetDate,
+    GetBindQun,
+    LooseNumber,
+    GetyamlValue,
+    GetTextJsonData,
+    PostTextJsonData,
+    GetYamlArrayRandomValue
+};
