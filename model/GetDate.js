c30e3ca5d043e4367748526d62cc28c61b5b1967
最好喝的小米粥 <admin@cunyx.cn>
2023-10-15 05:51:54 +0000

    呜呜呜呜
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- model/GetDate.js
+++ model/GetDate.js
@@ -5,6 +5,6 @@ export function GetDate (s) {
     let month = String(date.getMonth() + 1).padStart(2, '0');
     let day = String(date.getDate()).padStart(2, '0');
     let DateNow = s.replace(/Y/g,year).replace(/m/g,month).replace(/d/g,day);
-    Bot.logger.info("获取成功，返回"+DataNow);
+    Bot.logger.info("获取成功，返回"+DateNow);
     return DateNow;
 }
