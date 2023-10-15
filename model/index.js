1ebfcef8e4bfefdb13d34c7f39979abb835e6c1e
最好喝的小米粥 <admin@cunyx.cn>
2023-10-15 03:44:50 +0000

    报错看着好难受，不如把他修了吧
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- model/index.js
+++ model/index.js
@@ -1,6 +1,6 @@
 /**
  * @最好喝的小米粥
- * By 2023-10-14 18:07
+ * By 2023-10-15 11:44
  * http://afdian.net/a/woxmz
  * 集合本文件夹下所有模型及函数
  */
@@ -9,7 +9,7 @@ import { random } from './random.js';
 import { GetDate } from './GetDate.js';
 import { GetBindQun } from './GetBindQun.js';
 import { LooseNumber } from './LooseNumber.js';
-import { GetYamlValue } from './GetyamlValue.js';
+import { GetYamlValue } from './GetYamlValue.js';
 import { SendForwardMsg } from './SendForwardMsg.js';
 import { GetTextJsonData } from './GetTextJsonData.js';
 import { PostTextJsonData } from './PostTextJsonData.js';
