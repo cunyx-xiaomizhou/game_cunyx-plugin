2f41687d748b66e29e91e2a73ee70ab4c63adba3
最好喝的小米粥 <admin@cunyx.cn>
2023-10-15 03:57:16 +0000

    rename apps/profile.js to apps/MyMsg.js.
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- apps/MyMsg.js
+++ apps/MyMsg.js
@@ -2,17 +2,19 @@ import CIP from './../model/index.js';
 import plugin from './../../../lib/plugins/plugin.js';
 //定义全局变量
 let qq;
-export class CunyxImpart_profile extends plugin {
+export class CunyxImpart_MyMsg extends plugin {
   constructor () {
     super({
-      name:"寸幼萱淫趴:我的面板",
+      name:"寸幼萱淫趴:我的信息",
       dsc:"查看我的数据",
       event:"message",
       priority:1,/*优先级*/
       rule:[
-        {reg:"^#?profile$",fnc:"profile"}
+        {reg:"^#?淫趴信息$",fnc:"msg"}
       ]
     });
   }
-  //我放鸽子怎么了
+  async msg (e) {
+    e.reply("相应")
+  }
 }
