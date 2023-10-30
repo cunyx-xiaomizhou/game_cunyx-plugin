199a829c07e7df7c1c8ce031d6b39a3e19b363e7
最好喝的小米粥 <admin@cunyx.cn>
2023-10-30 09:39:10 +0000

    阿巴巴巴
    Signed-off-by: 最好喝的小米粥 <admin@cunyx.cn>

--- model/start.js
+++ model/start.js
@@ -10,6 +10,19 @@ export function start (e,IsData,FileName) {
     } else {
       dir = Bot.uin;
     }
+    let filePath = './plugins/impart_cunyx-plugin/data/'+dir+'/'+FileName+'.json';
+    if (!fs.existsSync(filePath)) {
+      let content = {};
+      fs.writeFileSync(filePath, JSON.stringify(content));
+      let Msg = GetYamlArrayRandomValue(e,"system","start").replace(/{_path_}/,"data/"+dir+"/"+FileName+".json");
+      Bot.logger.info("[寸幼萱淫趴][model/start]"+Msg);
+      e.reply(Msg,true);
+    } else {
+      let Msg = GetYamlArrayRandomValue(e,"system","start_already");
+      Bot.logger.info("[寸幼萱淫趴][model/start]"+Msg);
+      return true;
+    }
+    /**
     if (fs.existsSync('./plugins/impart_cunyx-plugin/data/'+dir+'/'+FileName+".json")) {
       let Msg = GetYamlArrayRandomValue(e,"system","start_already");
       Bot.logger.info("[寸幼萱淫趴][model/start]"+Msg);
@@ -21,7 +34,7 @@ export function start (e,IsData,FileName) {
         Bot.logger.info("[寸幼萱淫趴][model/start]"+Msg);
         e.reply(Msg,true);
       });
-    }
+    }*/
   } catch (err) {
     Bot.logger.error("[寸幼萱淫趴][model/start]错误信息如下：\n"+err);
     e.reply("文件初始化失败，错误已打印至控制台，请检查",true);
