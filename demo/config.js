f76be1c24fb66a133ba92e629ba82fb0707d13c9
最好喝的小米粥 <admin@cunyx.cn>
2024-07-16 08:08:45 +0000

    删除文件 demo/config.js

--- demo/config.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-async function config(fileName, key) {
-  const config_path = process.cwd() + '/plugins/impart_cunyx-plugin/config/config/';
-  let filePath = config_path + fileName + '.json';
-  let content = await fs.readFileSync(filePath);
-  let Json = await JSON.parse(content);
-  return Json.config[key];
-}
-export { config };
